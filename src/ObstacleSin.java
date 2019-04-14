084357d3b858a6dbbecb4cf44c15a192679f605b
renau <RenaudGirard>
2019-04-14 17:28:59 -0400

    ajout de deux classes types d'obstacle. Une d'enter elles non nécessaire (simple). À vérif

---
+++ src/ObstacleSin.java
@@ -0,0 +1,39 @@
+public class ObstacleSin extends Obstacle {
+
+    private double omega;
+    private double amplitude;
+    private double origine;
+
+
+
+    public ObstacleSin(double x, double origine ,double r, double vx, double omega,double amp){
+        super(x,origine,r,vx,1.0,0);
+        this.omega=omega;
+        amplitude=amp;
+        this.origine=origine;
+    }
+
+    @Override
+    public void update(double dt){
+
+        setX(getX()+dt*getVx());
+        setY(origine+amplitude*Math.sin(omega*dt));
+
+    }
+
+    public double getOmega(){
+        return omega;
+    }
+
+    public void setOmega(double u){
+        omega=u;
+    }
+
+    public double getAmplitude(){
+        return amplitude;
+    }
+
+    public void setAmplitude(double u){
+        amplitude=u;
+    }
+}
