562fdff917f1a0685def38c43e1071a5e3b54870
renau <RenaudGirard>
2019-04-15 15:24:47 -0400

    ajout de la fonction depasse

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,18 +1,34 @@
 public class Obstacle extends Sphere {
 
-
-    public Obstacle(double x, double y, double r, double vx, double vy, double ay){ super(x,y,r,vx,vy,ay); }
-
-    public void update(double dt){
-
-        setVy(getVy()+getAy()*dt);
+    private boolean depasse = false;
 
 
-        setX(getX()+getVx()*dt);
-        setY(getY()+getVy()*dt);
+    public Obstacle(double x, double y, double r, double vx, double vy, double ay) {
+        super(x, y, r, vx, vy, ay);
+    }
+
+    public void update(double dt) {
+
+        setVy(getVy() + getAy() * dt);
+
+
+        setX(getX() + getVx() * dt);
+        setY(getY() + getVy() * dt);
+
+    }
+
+    public boolean getDepasse() {
+        return depasse;
+    }
+
+    private void setDepasse(Fantome boo) {
+
+        if ((boo.getX()-boo.getR()) > (this.getX()+this.getR())) {
+            depasse = true;
+        }
 
     }
 
 
-
 }
+
