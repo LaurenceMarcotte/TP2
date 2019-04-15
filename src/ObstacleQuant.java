32164930e55ec67229503e310c9af93e1b3f934b
renau <RenaudGirard>
2019-04-15 15:04:49 -0400

    fonction quantique modif

--- src/ObstacleQuant.java
+++ src/ObstacleQuant.java
@@ -1,5 +1,7 @@
 public class ObstacleQuant extends Obstacle {
 
+    private int compteur;
+
     private double origine;
 
     public ObstacleQuant(double origine, double y, double r, double vx, double ay) {
@@ -11,18 +13,24 @@ public class ObstacleQuant extends Obstacle {
 
     private void quantique(boolean ordre) {
 
-        double aleatoire= Math.random()*60 -30;
+        double aleatoireX= Math.random()*60 -30;
+        double aleatoireY=Math.random()*60-30;
 
         if (ordre){
 
-            setX(origine+aleatoire+Math.signum(aleatoire)*getR());
+            setX(origine+aleatoireX+Math.signum(aleatoireX)*getR());
+            setY(origine+aleatoireY+Math.signum(aleatoireY)*getR());
 
-        }else{ setY(origine+aleatoire+Math.signum(aleatoire)*getR());}
+        }
 
     }
 
-    public void update(){
-        //pt un compteur pour faire le quantum ?
+
+    @Override
+    public void update(double dt){
+        super.update(dt);
+        compteur++;
+        quantique(compteur%3==0);
 
     }
 
