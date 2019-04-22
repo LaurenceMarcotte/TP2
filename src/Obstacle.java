bcd97119b218987690b28b11044398ec9e24ece0
LaurenceM <laurencemarcotte@hotmail.ca>
2019-04-22 16:06:53 -0400

    More Javadoc

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,12 +1,25 @@
 public class Obstacle extends Sphere {
 
-    private boolean depasse = false;
+    private boolean depasse = false; //indique si l'obstacle a été dépassé
 
-
+    /**
+     * Création d'un obstacle vu comme une sphère
+     *
+     * @param x position initiale en x
+     * @param y position initiale en y
+     * @param r rayon de l'obstacle
+     * @param vx vitesse en x initiale
+     * @param vy vitesse en y initiale
+     * @param ay accélération en y
+     */
     public Obstacle(double x, double y, double r, double vx, double vy, double ay) {
         super(x, y, r, vx, vy, ay);
     }
 
+    /**
+     * Mise à jour de la position d'un obstacle
+     * @param dt temps écoulé depuis la dernière mise à jour
+     */
     public void update(double dt) {
 
         setVy(getVy() + getAy() * dt);
@@ -16,12 +29,20 @@ public class Obstacle extends Sphere {
         setY(getY() + getVy() * dt);
     }
 
+    /**
+     * Getter de l'attribut depasse
+     * @return boolean depasse, si vrai obstacle a été dépassé, faux sinon
+     */
     public boolean getDepasse() {
         return depasse;
     }
 
+    /**
+     * Setter de depasse
+     * @param boo fantôme qui peut dépasser l'obstacle
+     */
     public void setDepasse(Fantome boo) {
-
+        //vérifie si le fantôme est plus loin que l'obstacle
         if ((boo.getX()-boo.getR()) > (this.getX()+this.getR())) {
             depasse = true;
         }
--- src/ObstacleSin.java
+++ src/ObstacleSin.java
@@ -1,12 +1,21 @@
 public class ObstacleSin extends Obstacle {
 
-    private double omega;
-    private double amplitude;
-    private double origine;
-    private double tempsEcoule;
+    private double omega; //vitesse angulaire
+    private double amplitude; //amplitude du sinus
+    private double origine; //position initiale en y, centre du sinus
+    private double tempsEcoule; //temps écoulé depuis la création de l'obstacle
 
 
-
+    /**
+     * Génère un obstacle sinusoïdal
+     *
+     * @param x position initiale en x
+     * @param origine position initiale en y, centre du sinus
+     * @param r rayon de l'obstacle
+     * @param vx vitesse initiale en x
+     * @param omega vitesse angulaire du sinus
+     * @param amp amplitude du sinus
+     */
     public ObstacleSin(double x, double origine ,double r, double vx, double omega,double amp){
         super(x,origine,r,vx,1.0,0);
         this.omega=omega;
@@ -14,6 +23,10 @@ public class ObstacleSin extends Obstacle {
         this.origine=origine;
     }
 
+    /**
+     * Mise à jour de la position du l'obstacle sinus
+     * @param dt temps écoulé depuis la dernière mise à jour
+     */
     @Override
     public void update(double dt){
         tempsEcoule+=dt;
@@ -22,18 +35,34 @@ public class ObstacleSin extends Obstacle {
 
     }
 
+    /**
+     * Getter de la vitesse angulaire
+     * @return vitesse angulaire
+     */
     public double getOmega(){
         return omega;
     }
 
+    /**
+     * Setter de la vitesse angulaire
+     * @param u nouvelle vitesse angulaire
+     */
     public void setOmega(double u){
         omega=u;
     }
 
+    /**
+     * Getter de l'amplitude du sinus
+     * @return amplitude du sinus
+     */
     public double getAmplitude(){
         return amplitude;
     }
 
+    /**
+     * Setter de l'amplitude du sinus
+     * @param u nouvelle amplitude du sinus
+     */
     public void setAmplitude(double u){
         amplitude=u;
     }
