0d59a52291dbaabac6a624455510d617bcb57d89
LaurenceM <laurencemarcotte@hotmail.ca>
2019-04-22 15:45:28 -0400

    More Javadoc

--- src/Modele.java
+++ src/Modele.java
@@ -28,9 +28,9 @@ public class Modele {
     /**
      * Sert à mettre à jour la position du fantôme.
      *
-     * @param deltaTime
-     * @param height
-     * @return
+     * @param deltaTime Temps écoulé depuis la dernière mise à jour
+     * @param height hauteur du jeu
+     * @return un tableau de double qui a comme format {positionXFantome, positionYFantome, déplacementXFantome}
      */
     public double[] updateGhost(double deltaTime, int height){
         ghost.update(deltaTime, height);
@@ -42,41 +42,46 @@ public class Modele {
         return positions;
     }
 
+    /**
+     * Ajuste la vitesse du fantome à 300 vers le haut, devrait être appelé quand on appuie sur espace
+     */
     public void vitesseGhost(){
         ghost.setVy(-300);
     }
 
+    /**
+     * Met à jour les positions de tous les obstacles en jeu. Commence par crééer des obstacles si 3 secondes s'est
+     * écoulé depuis la dernière création d'obstacle.
+     *
+     * @param dt temps écoulé depuis la dernière mise à jour
+     * @param width largeur de la fenêtre de jeu
+     * @param height hauteur de la fenêtre de jeu
+     * @return nouvelles positions des obstacles dans un hashmap qui associe les positions avec le numéro de l'osbtacle
+     */
     public HashMap<Integer, double[]> updateObstacle(double dt, int width, int height){
         dtObstacle+=dt;
+
+        //On crée un nouvel obstacle à chaque 3 secondes
         if(dtObstacle>=3){
             creerObstacle();
             dtObstacle=0;
         }
 
-        HashMap<Integer, double[]> o = new HashMap<>();
-
+        //mise à jour des positions des obstacles ici
         for (Obstacle obs: obstacles.values()) {
             obs.update(dt);
-            if(!obs.getDepasse()){
-                obs.setDepasse(ghost);
-                if(obs.getDepasse()){
-                    score+=5;
-                    obstacleEvite++;
-                    ghost.setVx(ghost.getVx()+15);
-                    if(obstacleEvite == 2){
-                        ghost.setAy(ghost.getAy() + 15);
-                        obstacleEvite=0;
-                    }
-                }
-            }
+            obstacleDepasse(obs); //vérifie si l'osbtacle a été dépassé
 
-            //obstacle est rendu hors de la fenêtre à gauche
+            //obstacle est rendu hors de la fenêtre à gauche, on le retire/détruit
             if(obs.getDepasse() && obs.getX() + obs.getR() - ghost.getX() < - width/2){
                 obstacles.remove(obs);
             }
-
         }
 
+        //va contenir les nouvelles positions de chaque obstacle
+        HashMap<Integer, double[]> o = new HashMap<>();
+
+        //ajout des nouvelles positions des obstacles dans o
         for (Integer i: obstacles.keySet()) {
             double[] pos = new double[3];
             Obstacle obs = obstacles.get(i);
@@ -89,27 +94,33 @@ public class Modele {
         return o;
     }
 
+    /**
+     * Création des nouveaux obstacles ici avec leur position initiale
+     */
     public void creerObstacle(){
 
-        double rayon= Math.floor(Math.random()*35)+10; // regarder si la valeur est ok
+        double rayon= Math.floor(Math.random()*35)+10; // Définie le rayon de l'obstacle
 
-        //on va utiliser un switch pour l'analyse du type d'objet
 
-        int objet= (int)Math.ceil(Math.random()*3); // le 3 représente le nb de types d'obstacles. à changer
-
-        double hauteur= Math.random()*(400-2*rayon)+rayon;
+        /* on va utiliser un switch pour l'analyse du type d'objet.
+         * Le 3 représente le nb de types d'obstacles. Si c'est 0, on a un obstacle simple, si c'est 1, un obstacle
+         * sinus, si c'est 3 un obstacle quantique.
+         */
+        int objet= (int)Math.ceil(Math.random()*3); //
 
         Obstacle obstacle;
 
-        double x= ghost.getX()+320+rayon;
-        double y= hauteur;
-
-        //la vitesse n'est pas set ici. on la set en fonction du déroulement du jeu
+        /*
+         * Position initiale de l'osbtacle. On l'initialise en x juste à l'extérieur de la fenêtre.
+         * En y, on le place de façon aléatoire.
+         */
+        double x = ghost.getX()+320+rayon;
+        double y = Math.random()*(400-2*rayon)+rayon;
 
         switch(objet){
 
             //obstacle simple
-            case 1: obstacle=new ObstacleSimple(x,y,rayon,0,0);
+            case 1: obstacle=new Obstacle(x,y,rayon,0,0,0);
                     obstacles.put(numeroObstacle, obstacle);
                     numeroObstacle++;
                     break;
@@ -126,19 +137,51 @@ public class Modele {
                     numeroObstacle++;
                     break;
         }
-
     }
 
+    /**
+     * Getter de l'attribut score qui contient le score du joueur
+     * @return le score du joueur
+     */
     public int getScore(){
         return score;
     }
 
+    /**
+     * Vérification des collisions entre le fantôme et les obstacles.
+     *
+     * @return un HashMap qui contient true or false dépendamment de si le fantôme est entré en collision avec un
+     * obstacle.
+     */
     public HashMap<Integer, Boolean> testCollision(){
         HashMap<Integer, Boolean> collision = new HashMap();
+
         for (Integer i: obstacles.keySet()) {
             collision.put(i, ghost.intersects(obstacles.get(i)));
         }
+
         return collision;
+    }
+
+    /**
+     *
+     * @param obs
+     */
+    public void obstacleDepasse(Obstacle obs){
+        if(!obs.getDepasse()){
+            obs.setDepasse(ghost);
+
+            if(obs.getDepasse()){
+                score+=5;
+                obstacleEvite++;
+                ghost.setVx(ghost.getVx()+15);
+
+                if(obstacleEvite == 2){
+                    ghost.setAy(ghost.getAy() + 15);
+                    obstacleEvite=0;
+                }
+            }
+        }
     }
 
 }
